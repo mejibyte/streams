3f8d8b3adb6509035da67bd32018abce47e94aff
Andrés Mejía <andresmejia@gmail.com>
2020-08-23 16:29:34 -0700

    Add assets from today's stream.

---
+++ 2020-08-23/estrategia.md
@@ -0,0 +1,9 @@
+# Estrategia patentada de Andrés
+
+1. Leer o escuchar cuidadosamente el problema
+2. Escribir más ejemplos y explicarle los ejemplos al entrevistador
+3. Pensar en casos degenerados ("edge cases" ó "corner cases")
+4. Escribir el problema en términos matemáticos
+5. Encontrar la solución más ingenua que se te ocurra
+6. Si es necesario, pensar en una solución mejor
+
--- 2020-08-23/solution1.cpp
+++ 2020-08-23/solution1.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+void solveCase() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+
+  for (int i = 0; i < n; ++i) {
+    // Find j that satisfies our conditions.
+    int answer = -1;
+    for (int j = i + 1; j < n; ++j) {
+      // Test if j satisfies the 3 conditions.
+      if (a[i] < a[j]) {
+        answer = a[j];
+        break;
+      }
+    }
+    cout << " " << answer;
+  }
+  cout << endl;
+}
+
+
 int main() {
-  // TODO(mejibyte): Write me.
+  int C;
+  cin >> C;
+  for (int caseNumber = 1; caseNumber <= C; ++caseNumber) {
+    cout << "Case #" << caseNumber << ":";
+    solveCase();
+  }
   return 0;
 }
